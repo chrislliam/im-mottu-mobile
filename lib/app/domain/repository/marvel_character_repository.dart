f379df8002062758016e622eb2d385e563018073
chrislliam <91385593+chrislliam@users.noreply.github.com>
2024-09-01 14:54:04 -0300

    Create marvel_character_repository.dart

---
+++ lib/app/domain/repository/marvel_character_repository.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+import '../../core/features/errors/custom_exception.dart';
+import '../entities/character_overview_entity.dart';
+import '../entities/character_preview_entity.dart';
+import '../enum/character_by_content_type.dart';
+
+abstract interface class MarvelCharacterRepository {
+  Future<Either<CustomException, List<CharacterPreviewEntity>>> getCharacters(
+      int offset);
+
+  Future<Either<CustomException, List<CharacterPreviewEntity>>>
+      getFilteredLCharactersList(CharacterByContentType filter, int id, offset);
+
+  Future<Either<CustomException, List<CharacterPreviewEntity>>>
+      searchCharacters(String name);
+
+  Future<Either<CustomException, CharacterOverviewEntity>> getCharacter(int id);
+}
