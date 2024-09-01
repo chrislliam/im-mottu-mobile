0a098fe5cf2b2c908da2f005dfff12d115105848
chrislliam <91385593+chrislliam@users.noreply.github.com>
2024-09-01 14:51:08 -0300

    Add CharacterPreviewEntity
    Added CharacterPreviewEntity for representation in the character list;

---
+++ lib/app/domain/entities/character_preview_entity.dart
@@ -0,0 +1,8 @@
+class CharacterPreviewEntity {
+  final int id;
+  final String name;
+  final String thumbnail;
+
+  CharacterPreviewEntity(
+      {required this.id, required this.name, required this.thumbnail});
+}
