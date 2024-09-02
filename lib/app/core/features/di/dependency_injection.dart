a93d890c22667371f9173b0235460450c5e82162
chrislliam <91385593+chrislliam@users.noreply.github.com>
2024-09-01 23:49:06 -0300

    Replaced HttpClient to CoreHttpClient
    replaced HttpClient to CoreHttpClient

--- lib/app/core/features/di/dependency_injection.dart
+++ lib/app/core/features/di/dependency_injection.dart
@@ -1,19 +1,57 @@
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import '../../../data/datasources/marvel_character_datasource.dart';
+import '../../../data/repository/marvel_character_repository_impl.dart';
+import '../../../domain/repository/marvel_character_repository.dart';
+import '../../../domain/usecases/get_character_by_id_usecase.dart';
+import '../../../domain/usecases/get_characters_usecase.dart';
+import '../../../domain/usecases/get_filtered_characters_list_usecase.dart';
+import '../../../domain/usecases/search_characters_usecase.dart';
+import '../constants/constants.dart';
 import '../http_client/http_client.dart';
 import '../../network/network_info.dart';
 import '../../network/network_info_datasource.dart';
 
 class DependencyInjection {
   static void init() {
-    Get.lazyPut<HttpClient>(() => DioHttpClient());
+    // Core
+    Get.lazyPut<CoreHttpClient>(() => DioHttpClient());
 
+    // Datasources
     Get.lazyPut<NetworkInfoDatasource>(
       () => NetworkInfoDatasourceImpl(
-        const EventChannel('com.mottu.marvel.im_mottu_mobile/connectivity/status'),
+        const EventChannel(Constants.connectivityEventChannel),
+        const MethodChannel(Constants.connectivityMethodChannel),
       ),
     );
 
+    Get.lazyPut<MarvelCharacterDataSource>(
+      () => MarvelCharacterDataSourceImpl(
+        Get.find<NetworkInfoDatasource>(),
+        Get.find<CoreHttpClient>(),
+      ),
+    );
+
+    // Repositories
+    Get.lazyPut<MarvelCharacterRepository>(
+      () => MarvelCharacterRepositoryImpl(Get.find<MarvelCharacterDataSource>()),
+    );
+
+    // Use Cases
+    Get.lazyPut<GetCharactersUsecase>(
+      () => GetCharactersUsecase(Get.find<MarvelCharacterRepository>()),
+    );
+    Get.lazyPut<SearchCharactersUsecase>(
+      () => SearchCharactersUsecase(Get.find<MarvelCharacterRepository>()),
+    );
+    Get.lazyPut<GetFilteredCharactersListUsecase>(
+      () => GetFilteredCharactersListUsecase(Get.find<MarvelCharacterRepository>()),
+    );
+    Get.lazyPut<GetCharacterByIdUsecase>(
+      () => GetCharacterByIdUsecase(Get.find<MarvelCharacterRepository>()),
+    );
+
+    //Controllers
     Get.lazyPut<NetworkInfo>(
       () => NetworkInfo(Get.find<NetworkInfoDatasource>()),
     );
