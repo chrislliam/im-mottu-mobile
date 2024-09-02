e1b04c3d3af6201ee997c307c3860c642e2f7cf0
chrislliam <91385593+chrislliam@users.noreply.github.com>
2024-09-01 23:44:37 -0300

    Update network_info_datasource.dart
    added method channel

--- lib/app/core/network/network_info_datasource.dart
+++ lib/app/core/network/network_info_datasource.dart
@@ -1,15 +1,35 @@
+import 'dart:io';
+
 import 'package:flutter/services.dart';
 
 abstract interface class NetworkInfoDatasource {
   Stream<bool> get onConnectivityChanged;
+
+  Future<bool> get isConnected;
 }
 
 class NetworkInfoDatasourceImpl implements NetworkInfoDatasource {
   final EventChannel eventChannel;
+  final MethodChannel methodChannel;
 
-  NetworkInfoDatasourceImpl(this.eventChannel);
+  NetworkInfoDatasourceImpl(this.eventChannel, this.methodChannel);
 
   @override
-  Stream<bool> get onConnectivityChanged =>
-      eventChannel.receiveBroadcastStream().cast<bool>();
+  Stream<bool> get onConnectivityChanged {
+    if (Platform.isAndroid) {
+      return eventChannel.receiveBroadcastStream().cast<bool>();
+    } else {
+      return Stream.value(true);
+    }
+  }
+
+  @override
+  Future<bool> get isConnected async {
+    if (Platform.isAndroid) {
+      final bool result = await methodChannel.invokeMethod('isConnected');
+      return result;
+    } else {
+      return true;
+    }
+  }
 }
