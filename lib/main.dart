49123daa4c547e49ce67db4680c45904bd8ef115
chrislliam <91385593+chrislliam@users.noreply.github.com>
2024-09-02 19:22:23 -0300

    Update main.dart
    replaced MaterialApp to GetMaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'app/core/features/di/dependency_injection.dart';
 import 'app/core/features/local_storage/local_storage.dart';
+import 'app/presentation/pages/character_overview_page.dart';
 import 'app/presentation/pages/home_page.dart';
+import 'app/presentation/pages/splash_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,12 +23,17 @@ class AppWidget extends StatefulWidget {
 class _AppWidgetState extends State<AppWidget> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Mottu Marvel',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      initialRoute: '/home',
+      getPages: [
+        GetPage(name: '/', page: () => const SplashPage()),
+        GetPage(name: '/home', page: () => const HomePage()),
+        GetPage(
+            name: '/character_overview',
+            page: () => const CharacterOverviewPage())
+      ],
     );
   }
 }
