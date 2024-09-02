0a3edc1c947729462ce3d0d066688cec7a639550
chrislliam <91385593+chrislliam@users.noreply.github.com>
2024-09-01 23:38:46 -0300

    created eventChannel and MethodChannel for ios
    reate NetworkInfoMethodChannel and NetworkInfoEventChannel for iOS to Handle Connectivity Status

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,11 +3,37 @@ import UIKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+
+    private var eventChannel: FlutterEventChannel?
+    private var methodChannel: FlutterMethodChannel?
+
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
+        
+        eventChannel = FlutterEventChannel(name: "com.mottu.marvel.im_mottu_mobile/connectivity/event",
+                                           binaryMessenger: controller.binaryMessenger)
+        eventChannel?.setStreamHandler(NetworkInfoEventChannel())
+
+        methodChannel = FlutterMethodChannel(name: "com.mottu.marvel.im_mottu_mobile/connectivity/method",
+                                             binaryMessenger: controller.binaryMessenger)
+        
+        methodChannel?.setMethodCallHandler { [weak self] (call, result) in
+            self?.handleMethodCall(call: call, result: result)
+        }
+
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+
+    private func handleMethodCall(call: FlutterMethodCall, result: @escaping FlutterResult) {
+        if call.method == "isConnected" {
+            let isConnected = NetworkInfoMethodChannel().isConnectedToNetwork() 
+            result(isConnected)
+        } else {
+            result(FlutterMethodNotImplemented)
+        }
+    }
 }
