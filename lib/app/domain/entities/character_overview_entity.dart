2a5b7c1f08aae8b134a69ba9a4ad94e3f78bea31
chrislliam <91385593+chrislliam@users.noreply.github.com>
2024-09-01 23:43:51 -0300

    Update character_overview_entity.dart

--- lib/app/domain/entities/character_overview_entity.dart
+++ lib/app/domain/entities/character_overview_entity.dart
@@ -3,6 +3,6 @@ import 'character_preview_entity.dart';
 class CharacterOverviewEntity extends CharacterPreviewEntity {
   final String description;
 
-  CharacterOverviewEntity(this.description,
-      {required super.id, required super.name, required super.thumbnail});
+  CharacterOverviewEntity(
+      {required super.id, required super.name, required super.thumbnail,required this.description,});
 }
